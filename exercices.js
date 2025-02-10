27a22976b4fe26da296066525e86f5b44481c6b0
Ruzanna <ruzannancy@gmail.com>
2025-02-10 17:29:25 +0100

    Des informations manquent !

--- exercices.js
+++ exercices.js
@@ -1,3 +1,12 @@
 // Quel est le titre de la boutique
 const h1 = document.querySelector("h1");
 console.log(h1.textContent);
+
+// Des informations manquent !
+
+const description_boutique = document.querySelector("#description_boutique");
+const new_p = document.createElement("p");
+new_p.textContent =
+  "Pour cela, il vous suffit de cliquer sur une des potions de la liste ci-dessous pour l'acheter.";
+
+description_boutique.appendChild(new_p);
